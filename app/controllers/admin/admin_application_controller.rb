757321024b169784207156c91507ed9eb9c7f8bd
Michael S Trumpf <miketrumpf@gmail.com>
2017-03-15 21:11:19 -0400

    admin lte foundation set up

--- app/controllers/admin/admin_application_controller.rb
+++ app/controllers/admin/admin_application_controller.rb
@@ -7,7 +7,7 @@ class Admin::AdminApplicationController < ActionController::Base
   end
 
   def current_admin
-    @current_admin ||= Administrator.find(session[:admin_id]) if session[:admin_id]
+    @current_admin ||= Admin.find(session[:admin_id]) if session[:admin_id]
   end
   helper_method :current_admin
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace 'admin' do
+    resources :sessions, only: [:new, :create, :destroy]
+    get 'login', to: 'sessions#new', as: :login
+    get 'logout', to: 'sessions#destroy', as: :logout
+    root 'dashboard#index'
+    resources :posts
+  end
+
+  namespace :api do
+  ##POC routes no sessions
+    namespace :v1 do
+      resources :posts, only: [:index]
+    end
+  end
 end
