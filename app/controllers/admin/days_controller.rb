6787dcb3c2bfca0ce18b0d668d2ed81875dde853
Michael S Trumpf <miketrumpf@gmail.com>
2017-05-24 15:10:11 -0400

    admin css changes

--- app/controllers/admin/days_controller.rb
+++ app/controllers/admin/days_controller.rb
@@ -1,6 +1,8 @@
 class Admin::DaysController < Admin::AdminApplicationController
   def index
-    @days = Day.all
+    today = Day.where(date: Date.today).last
+    # @days = Day.include("date").where("date BETWEEN (?) AND (?)", today.date - 30.days, today.date + 30.days)
+    @days = Day.where("date BETWEEN (?) AND (?)", today.date - 30.days, today.date + 30.days)
   end
 
   def show
