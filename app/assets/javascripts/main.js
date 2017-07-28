9deca9a8d79b7074fe0b29e0e7c0e5f66bf56cde
Michael S Trumpf <miketrumpf@gmail.com>
2017-07-28 12:26:19 -0400

    any logo change.  Month up and down bug fixed.  It was a turbolinks issue

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -2,35 +2,41 @@ $(function() {
 
   console.log("main js loaded")
 
-  $("body").on("click", "#month-down", function(e){
-    e.preventDefault();
-    var month = $(this).data("month")
-    var year = $(this).data("year")
 
-    $.ajax({
-      url: '/admin/month_down',
-      dataType: 'script',
-      data: {month: month, year: year}
+  $(document).on('turbolinks:load', function() {
+
+
+    $("body").on("click", "#month-down", function(e){
+      e.preventDefault();
+      var month = $(this).data("month")
+      var year = $(this).data("year")
+
+      $.ajax({
+        url: '/admin/month_down',
+        dataType: 'script',
+        data: {month: month, year: year}
+      });
     });
-  });
 
 
 
-  $("body").on("click", "#month-up", function(e){
-    e.preventDefault();
-    var month = $(this).data("month")
-    var year = $(this).data("year")
+    $("body").on("click", "#month-up", function(e){
+      e.preventDefault();
+      var month = $(this).data("month")
+      var year = $(this).data("year")
 
 
-    $.ajax({
-      url: '/admin/month_up',
-      dataType: 'script',
-      data: {month: month, year: year},
+      $.ajax({
+        url: '/admin/month_up',
+        dataType: 'script',
+        data: {month: month, year: year},
 
-      // success: function(response) {
-      //   debugger
-      // }
+        // success: function(response) {
+        //   debugger
+        // }
+      });
     });
+    
   });
 
 
